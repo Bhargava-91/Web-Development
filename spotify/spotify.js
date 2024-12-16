a3ebb1e39e72c0c05c3fa5790505cfa29fbb8843
Bhargava-91 <bk.adapala@gmail.com>
2024-12-16 12:06:33 +0530

    spotify clone started

---
+++ spotify/spotify.js
@@ -0,0 +1,98 @@
+console.log("Java Script")
+let currentsong = new Audio();
+
+function secondsToMinutes(seconds){
+    if (isNaN(seconds) || seconds < 0) {
+        return "Invalid Input"
+    }
+    const minutes = Math.floor(seconds / 60)
+    const remainingSeconds = Math.floor(seconds % 60)
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0')
+
+    return `${formattedMinutes}:${formattedSeconds}`
+}
+
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:5501/Web-Development/spotify/songs/")
+    let response = await a.text();
+    console.log(response);
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split("/songs/")[1])
+        }
+    }
+    return songs
+}
+
+const playMusic= (track, pause = false)=>{
+    // let audio = new Audio("/spotify/songs/" + track)
+    currentsong.src = "/Web-Development/spotify/songs/" + track
+    if (!pause) {
+        currentsong.play()
+        play.src = "pause.svg"
+    }
+    document.querySelector(".songinfo").innerHTML = decodeURI(track)
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
+    
+}
+
+async function main() {
+
+    let songs = await getSongs()
+    playMusic(songs[0], true)
+
+    let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li data-song="${song.replaceAll("%20", " ")}"> <img class="invert" src="music.svg" alt="">
+                            <div class="info">
+                                <div>${song.replaceAll("%20", " ")} </div>
+                                <div>Bhargava</div>
+                            </div>
+                            <div class="playnow">
+                                <span>Play Now</span>
+                                <img class="invert" src="play.svg" alt="">
+                            </div> </li>`;
+    }
+
+    Array.from(document.querySelector(".songlist").getElementsByTagName("li")).forEach((e) => {
+        e.addEventListener("click", (element) => {
+            console.log(e.querySelector(".info").firstElementChild.innerHTML)
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+
+
+            // const songName = e.getAttribute("data-song"); // Get the song from data attribute
+            // console.log(songName);  // Print the exact song name
+            // playMusic(songName);    // Play the corresponding song
+        });
+    });
+
+    play.addEventListener("click", () =>{
+        if (currentsong.paused) {
+            currentsong.play()
+            play.src = "pause.svg"
+        }
+        else{
+            currentsong.pause()
+            play.src = "play.svg"
+
+        }
+    })
+
+    currentsong.addEventListener("timeupdate", () => {
+        console.log(currentsong.currentTime, currentsong.duration)
+        document.querySelector(".songtime").innerHTML = `${secondsToMinutes(currentsong.currentTime)}/${secondsToMinutes(currentsong.duration)}`
+        document.querySelector(".circle").style.left = (currentsong.currentTime/currentsong.duration)*100 + "%"
+    })
+
+}
+main()
+
+
+
